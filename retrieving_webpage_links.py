913f42f27b17752ff8f50d8086376013f6a63735
nomdebrew <nomdebrew@gmail.com>
2018-09-19 12:53:26 -0400

    first commit

---
+++ retrieving_webpage_links.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 02:44:19 2018
+
+@author: nomdebrew
+"""
+
+import urllib.request, urllib.parse, urllib.error, re
+from bs4 import BeautifulSoup
+
+#url = input('Enter a url - ') #would allow user to enter a url
+url = 'https://en.wikipedia.org/wiki/Austin_Lucas'
+print('\nBelow is the output for page:',url,"\n")
+
+print("\nUsing Regular Expressions\n")
+
+link_count = 0
+link = 'https://en.wikipedia.org/wiki/Austin_Lucas'
+
+fhand = urllib.request.urlopen(url)
+for line in fhand:
+    line = line.decode().strip()
+    match = re.findall('href="[^#].*?(?=")', line)
+    if match:
+        for i in match:
+            print(i)
+            link_count +=1
+
+print("\nTotal # of links:",link_count)
+
+
+
+
+
+print("\n\nUsing BeautifulSoup\n")
+
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup ('a')  # retrive all of the anchor tags
+print(len(tags)) #prints lenght of tag list
+for tag in tags:
+    print(tag.get('href', None)) #https://en.wikipedia.org/wiki/Austin_Lucas
